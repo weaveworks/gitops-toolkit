4a2c6ee5d8efa0fa274212f729ebc3f1dc730749
Lucas Käldström <lucas@weave.works>
2021-07-08 18:08:57 +0300

    Update unit tests after dependency/framing updates

--- pkg/serializer/serializer_test.go
+++ pkg/serializer/serializer_test.go
@@ -31,9 +31,10 @@ var (
 	ext1gv    = schema.GroupVersion{Group: groupname, Version: "v1alpha1"}
 	ext2gv    = schema.GroupVersion{Group: groupname, Version: "v1alpha2"}
 
-	intsb  = runtime.NewSchemeBuilder(addInternalTypes)
-	ext1sb = runtime.NewSchemeBuilder(registerConversions, addExternalTypes(ext1gv), v1_addDefaultingFuncs, registerOldCRD)
-	ext2sb = runtime.NewSchemeBuilder(registerConversions, addExternalTypes(ext2gv), v2_addDefaultingFuncs, registerNewCRD)
+	intsb   = runtime.NewSchemeBuilder(addInternalTypes)
+	ext1sb  = runtime.NewSchemeBuilder(registerConversions, addExternalTypes(ext1gv), v1_addDefaultingFuncs, registerOldCRD)
+	ext2sb  = runtime.NewSchemeBuilder(registerConversions, addExternalTypes(ext2gv), v2_addDefaultingFuncs, registerNewCRD)
+	yamlSep = []byte("---\n")
 )
 
 func v1_addDefaultingFuncs(scheme *runtime.Scheme) error {
@@ -252,59 +253,65 @@ var (
 	newCRDMeta  = metav1.TypeMeta{APIVersion: "foogroup/v1alpha2", Kind: "CRD"}
 	unknownMeta = runtime.TypeMeta{APIVersion: "unknown/v1", Kind: "YouDontRecognizeMe"}
 
-	oneSimple = []byte(`apiVersion: foogroup/v1alpha1
+	oneSimple = []byte(`---
+apiVersion: foogroup/v1alpha1
 kind: Simple
 testString: foo
 `)
-	simpleUnknownField = []byte(`apiVersion: foogroup/v1alpha1
+	simpleUnknownField = []byte(`---
+apiVersion: foogroup/v1alpha1
 kind: Simple
 testString: foo
 unknownField: bar
 `)
-	simpleDuplicateField = []byte(`apiVersion: foogroup/v1alpha1
+	simpleDuplicateField = []byte(`---
+apiVersion: foogroup/v1alpha1
 kind: Simple
 testString: foo
 testString: bar
 `)
-	unrecognizedVersion = []byte(`apiVersion: foogroup/v1alpha0
+	unrecognizedVersion = []byte(`---
+apiVersion: foogroup/v1alpha0
 kind: Simple
 testString: foo
 `)
-	unrecognizedGVK = []byte(`apiVersion: unknown/v1
+	unrecognizedGVK = []byte(`---
+apiVersion: unknown/v1
 kind: YouDontRecognizeMe
 testFooBar: true
 `)
-	oneComplex = []byte(`Int64: 0
+	oneComplex = []byte(`---
+Int64: 0
 apiVersion: foogroup/v1alpha1
 bool: false
 int: 0
 kind: Complex
 string: bar
 `)
-	simpleAndComplex = []byte(string(oneSimple) + "---\n" + string(oneComplex))
+	simpleAndComplex = []byte(string(oneSimple) + string(oneComplex))
 
-	testList = []byte(`apiVersion: v1
+	testList = []byte(`---
+apiVersion: v1
 kind: List
 items:
-- apiVersion: foogroup/v1alpha1
-  kind: Simple # Test comment
-  # Test comment
-  testString: foo
-- apiVersion: foogroup/v1alpha1
-  # Test 2 comment
-  kind: Complex
-  int: 5 # Test 2 comment
-- apiVersion: foogroup/v1alpha1
-  kind: Simple # Test 3 comment
-  testString: bar
+  - apiVersion: foogroup/v1alpha1
+    kind: Simple # Test comment
+    # Test comment
+    testString: foo
+  - apiVersion: foogroup/v1alpha1
+    # Test 2 comment
+    kind: Complex
+    int: 5 # Test 2 comment
+  - apiVersion: foogroup/v1alpha1
+    kind: Simple # Test 3 comment
+    testString: bar
 `)
 
-	simpleJSON = []byte(`{"apiVersion":"foogroup/v1alpha1","kind":"Simple","testString":"foo"}
-`)
-	complexJSON = []byte(`{"apiVersion":"foogroup/v1alpha1","kind":"Complex","string":"bar","int":0,"Int64":0,"bool":false}
-`)
+	simpleJSON  = []byte(`{"apiVersion":"foogroup/v1alpha1","kind":"Simple","testString":"foo"}`)
+	complexJSON = []byte(`{"apiVersion":"foogroup/v1alpha1","kind":"Complex","string":"bar","int":0,"Int64":0,"bool":false}`)
 
-	oldCRD = []byte(`# I'm a top comment
+	oldCRD = []byte(`---
+# I'm a top comment
 apiVersion: foogroup/v1alpha1
 kind: CRD
 metadata:
@@ -313,14 +320,16 @@ metadata:
 testString: foobar # Me too
 `)
 
-	oldCRDNoComments = []byte(`apiVersion: foogroup/v1alpha1
+	oldCRDNoComments = []byte(`---
+apiVersion: foogroup/v1alpha1
 kind: CRD
 metadata:
   creationTimestamp: null
 testString: foobar
 `)
 
-	newCRD = []byte(`# I'm a top comment
+	newCRD = []byte(`---
+# I'm a top comment
 apiVersion: foogroup/v1alpha2
 kind: CRD
 metadata:
@@ -329,7 +338,8 @@ metadata:
 otherString: foobar # Me too
 `)
 
-	newCRDNoComments = []byte(`apiVersion: foogroup/v1alpha2
+	newCRDNoComments = []byte(`---
+apiVersion: foogroup/v1alpha2
 kind: CRD
 metadata:
   creationTimestamp: null
@@ -434,8 +444,8 @@ func TestDecodeInto(t *testing.T) {
 		{"complex external", oneComplex, false, &runtimetest.ExternalComplex{}, &runtimetest.ExternalComplex{TypeMeta: complexv1Meta, String: "bar"}, false},
 		{"defaulted complex external", oneComplex, true, &runtimetest.ExternalComplex{}, &runtimetest.ExternalComplex{TypeMeta: complexv1Meta, String: "bar", Integer64: 5}, false},
 		{"defaulted complex internal", oneComplex, true, &runtimetest.InternalComplex{}, &runtimetest.InternalComplex{String: "bar", Integer64: 5}, false},
-		{"decode unknown obj into unknown", unrecognizedGVK, false, &runtime.Unknown{}, newUnknown(unknownMeta, unrecognizedGVK), false},
-		{"decode known obj into unknown", oneComplex, false, &runtime.Unknown{}, newUnknown(complexv1Meta, oneComplex), false},
+		{"decode unknown obj into unknown", unrecognizedGVK, false, &runtime.Unknown{}, newUnknown(unknownMeta, bytes.TrimPrefix(unrecognizedGVK, yamlSep)), false},
+		{"decode known obj into unknown", oneComplex, false, &runtime.Unknown{}, newUnknown(complexv1Meta, bytes.TrimPrefix(oneComplex, yamlSep)), false},
 		{"no unknown fields", simpleUnknownField, false, &runtimetest.InternalSimple{}, nil, true},
 		{"no duplicate fields", simpleDuplicateField, false, &runtimetest.InternalSimple{}, nil, true},
 		{"no unrecognized API version", unrecognizedVersion, false, &runtimetest.InternalSimple{}, nil, true},
@@ -520,7 +530,7 @@ func TestDecodeUnknown(t *testing.T) {
 		expected    runtime.Object
 		expectedErr bool
 	}{
-		{"Decode unrecognized kinds into runtime.Unknown", unrecognizedGVK, true, newUnknown(unknownMeta, unrecognizedGVK), false},
+		{"Decode unrecognized kinds into runtime.Unknown", unrecognizedGVK, true, newUnknown(unknownMeta, bytes.TrimPrefix(unrecognizedGVK, yamlSep)), false},
 		{"Decode known kinds into known structs", oneComplex, true, &runtimetest.ExternalComplex{TypeMeta: complexv1Meta, String: "bar"}, false},
 		{"No support for unrecognized", unrecognizedGVK, false, nil, true},
 	}
